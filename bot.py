1e2ec8c7219597921c9bf4e80b0f776ce99c7f6e
Brian Wu <killbread48@gmail.com>
2021-03-14 23:10:37 -0400

    repeat and say commands

--- bot.py
+++ bot.py
@@ -1,20 +1,34 @@
 import discord
 from discord.ext import commands, tasks
-from fun_commands.smol_fun_commands import _boop
+from fun_commands.smol_fun_commands import _boop, _repeat, _say
 from time import strftime, localtime
 
 
 client = commands.Bot(command_prefix='!')
 
+
 @client.event
 async def on_ready():
     print(f"bot started {strftime('%I:%M %p, %m/%d', localtime())}")  # use %c for a full, pre-made date
+
 
 @client.command()
 async def boop(ctx, who: discord.User):
     await _boop(ctx, who)
 
+
+@client.command()
+async def repeat(ctx):
+    await _repeat(ctx)
+
+
+@client.command()
+async def say(ctx):
+    await _say(ctx)
+
+
 with open("bot_key.txt", "r") as file:
     key = file.read().split()
+
 
 client.run(key[0])
--- fun_commands/smol_fun_commands.py
+++ fun_commands/smol_fun_commands.py
@@ -1,9 +1,31 @@
 import discord
 from discord.ext import commands
 
+
 async def _boop(ctx, who: discord.User):
     if who is None:
         await ctx.send("Who u boopin???")
         return
 
     await ctx.send(who.mention+" ***YOU*** have been booped!!!!!!")
+
+
+async def _repeat(ctx):
+    async with ctx.typing():
+        if len(str(ctx.message.content)) > 9:
+            if ctx.message.mention_everyone:
+                message = discord.utils.escape_mentions(str(ctx.message.content)[8:])
+            else:
+                message = str(ctx.message.content)[8:]
+            await ctx.send(message)
+
+
+async def _say(ctx):
+    async with ctx.typing():
+        if len(str(ctx.message.content)) > 6:
+            if ctx.message.mention_everyone:
+                message = discord.utils.escape_mentions(str(ctx.message.content)[5:])
+            else:
+                message = str(ctx.message.content)[5:]
+            await ctx.send(message)
+        await ctx.message.delete()
