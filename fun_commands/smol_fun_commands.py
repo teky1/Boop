026781278c031f68028572e3540372f4c15efa40
Teky1 <officialteky@gmail.com>
2021-03-14 23:01:09 -0400

    added !calc command

--- fun_commands/smol_fun_commands.py
+++ fun_commands/smol_fun_commands.py
@@ -1,4 +1,5 @@
 import discord
+import utilities.equation_interpreter as interpreter
 from discord.ext import commands
 
 async def _boop(ctx, who: discord.User):
@@ -7,3 +8,15 @@ async def _boop(ctx, who: discord.User):
         return
 
     await ctx.send(who.mention+" ***YOU*** have been booped!!!!!!")
+
+async def _calc(ctx):
+    args = ctx.message.content.split()
+    if len(args) == 1:
+        await ctx.send("**Correct Format**: !calc <equation>\n\n"
+                       "*This command is used to make/test basic PEMDAS calculations using Ortho's equation interpreter.*")
+
+    equation = ctx.message.content.replace("!calc ", "", 1)
+
+    answer = round(float(interpreter.interpret(values={}, equation=equation)), 4)
+
+    await ctx.send(f"```\n{equation}\nAnswer: {answer}\n```")
