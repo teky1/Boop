c79bd52791f6db8d2b1b290361fec535e80b08d8
Teky1 <officialteky@gmail.com>
2021-05-13 15:11:21 -0400

    'stuff

--- general_commands/bedwars_leaderboard.py
+++ general_commands/bedwars_leaderboard.py
@@ -2,8 +2,7 @@ import utilities.equation_interpreter as interpreter
 import utilities.hypixel_utils as hypixel
 
 async def _bwterms(ctx):
-    await ctx.send("The available data for BW is: level, wins, losses, final_kills, final_deaths, kills, deaths, "
-                   "beds_broken, beds_lost")
+    await ctx.send("The available data for BW is: level (star), wins (w), losses (l), final_kills (fk), final_deaths (fd), kills (k), deaths (d), beds_broken (bb), beds_lost (bl)")
 
 async def _bwscore(ctx, ign, equation):
     values = hypixel.getIGNBwStats(ign)
@@ -16,14 +15,25 @@ async def _bwscore(ctx, ign, equation):
 
 async def _bedwarsleaderboard(ctx, equation):
     async with ctx.typing():
+
+
         with open("data/registered_players.txt", "r") as player_file:
             contents = player_file.read().split()
         all_stats = hypixel.getAllPlayerBwStats(contents)
         lb = leaderboard(all_stats, equation)
         output = f"```\nBed Wars\n{equation} leaderboards:\n"
 
+        total_sum = 0
+
         for i in range(len(lb)):
             output+=f"{i+1}. {lb[i][0]} ({lb[i][1]})\n"
+            total_sum += lb[i][1]
+
+        if ctx.message.content.lower().endswith("-s"):
+            output += "\nTotal Sum: "+str(total_sum)+"\n"
+        elif ctx.message.content.lower().endswith("-a"):
+            output += "\nAverage: " + str(round(total_sum/len(lb), 3))+"\n"
+
         output += "```"
         await ctx.send(output)
 
